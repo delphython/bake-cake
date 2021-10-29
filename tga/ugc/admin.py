46d8e94843efd2ab31cd6c1171965b1567b31318
delphython <delphython@gmail.com>
2021-10-29 16:33:57 +0300

    Add models to admin

--- tga/ugc/admin.py
+++ tga/ugc/admin.py
@@ -3,14 +3,88 @@ from django.contrib import admin
 from .forms import ProfileForm
 from .models import Message
 from .models import Profile
+from .models import Levels
+from .models import Forms
+from .models import Topping
+from .models import Berries
+from .models import Decors
 
 
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
-    list_display = ('id', 'external_id', 'name')
+    list_display = (
+        "id",
+        "external_id",
+        "name",
+    )
     form = ProfileForm
 
 
 @admin.register(Message)
 class MessageAdmin(admin.ModelAdmin):
-    list_display = ('id', 'profile', 'text', 'created_at')
+    list_display = (
+        "id",
+        "profile",
+        "text",
+        "created_at",
+    )
+
+
+@admin.register(Levels)
+class LevelsAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "cost",
+    )
+    list_edit = (
+        "name",
+        "cost",
+    )
+
+
+@admin.register(Forms)
+class FormsAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "cost",
+    )
+    list_edit = (
+        "name",
+        "cost",
+    )
+
+
+@admin.register(Topping)
+class ToppingAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "cost",
+    )
+    list_edit = (
+        "name",
+        "cost",
+    )
+
+
+@admin.register(Berries)
+class BerriesAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "cost",
+    )
+    list_edit = (
+        "name",
+        "cost",
+    )
+
+
+@admin.register(Decors)
+class DecorsAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "cost",
+    )
+    list_edit = (
+        "name",
+        "cost",
+    )
